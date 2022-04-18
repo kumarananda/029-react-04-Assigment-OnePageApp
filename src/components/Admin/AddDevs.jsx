041b95ec150b4482b3907baaeaeb8c893e9cc9a4
kumarananda <coderanandakumar@gmail.com>
2022-04-18 17:10:01 +0600

    update

--- src/components/Admin/AddDevs.jsx
+++ src/components/Admin/AddDevs.jsx
@@ -2,14 +2,14 @@
 
 
 import React from 'react';
-import { Card, Col, Container, Row, Table, Button } from 'react-bootstrap';
+import { Card, Col, Container, Row, Table, Button, Form } from 'react-bootstrap';
 import  'boxicons';
 import './Admin.css'
 
 
 const AddDevs = () => {
   return (
-    <section className='my-3 addDevs'>
+    <section className='my-3 addDevs '>
         <Container>
             <Row>
 
@@ -31,11 +31,11 @@ const AddDevs = () => {
                         </Card.Body>
                     </Card>
                     <Card>
-                        <Card.Body>
-                        <h5>Name:Ananda Kumar Saha</h5>
-                        <h5>Email: anandasaha484@gmail.com</h5>
-                        <h5>Genger: Male</h5>
-                        <h5>Skill: MERN Stack</h5>
+                        <Card.Body >
+                        <h5>Name: <p>Ananda Kumar Saha</p></h5>
+                        <h5>Email: <p>anandasaha484@gmail.com</p></h5>
+                        <h6>Genger: Male</h6>
+                        <h6>Skill: MERN Stack</h6>
                         <hr />
                         <h6><i class='bx bxl-facebook'></i>:               </h6>
                         <h6><i class='bx bxl-linkedin' ></i>:               </h6>
@@ -44,6 +44,48 @@ const AddDevs = () => {
                         <h6><i class='bx bxl-youtube'></i>:               </h6>
                         </Card.Body>
                     </Card>
+                </Col>
+                <Col md={9}>
+                    <Card>
+                        <Card.Header>Add Developers Data</Card.Header>
+                        <Card.Body>
+                        <Form >
+                            <Row>
+                                <Col md={5}>
+
+                                </Col>
+                                <Col md={7}>
+                                    <div className="my-2">
+                                        <Form.Label>Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter name" />
+                                    </div>
+                                    <div className="my-2">
+                                        <Form.Label>User name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter user name" />
+                                    </div>
+                                    
+                                    <Form.Group className="mb-1" controlId="formBasicUserUame">
+                                        
+                                    </Form.Group>
+                                    <Form.Group className="mb-1" controlId="formBasicEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter email" />
+                                    </Form.Group>
+                                    <hr />
+                                    <input type="radio" id="Male" name='gender' value='Male' /> <label htmlFor="Male">Male</label> &nbsp;
+                                    <input type="radio" id="Female" name='gender'  value='Female' /> <label htmlFor="Female">Female</label>
+                                    <Form.Group className="mb-1" controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="Check me out" />
+                                    </Form.Group>
+                                <Button variant="primary" type="submit">Submit</Button>
+                                </Col>
+                            </Row>
+                            
+                        </Form>
+                        </Card.Body>
+                    </Card>
+
+
                 </Col>
             </Row>
         </Container>
