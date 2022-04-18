3c22cfcbd3ebed889a40a2225f7f0ccbf4823fe5
kumarananda <coderanandakumar@gmail.com>
2022-04-19 03:44:25 +0600

    form object

--- src/components/Admin/AddDevs.jsx
+++ src/components/Admin/AddDevs.jsx
@@ -1,7 +1,7 @@
 
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Col, Container, Row, Table, Button, Form } from 'react-bootstrap';
 import  'boxicons';
 import './Admin.css'
@@ -11,7 +11,26 @@ import { Link } from 'react-router-dom';
 
 const AddDevs = () => {
 
-    
+    // from input with useState
+    const [devinput, setDevinput ] = useState({
+        name : '',
+        uName : '',
+        cell: '',
+        photo : '',
+        email : '',
+        gender : '',
+        yt : '',
+        git : '',
+        tw : '',
+        lin : '',
+        fb :''
+
+    });
+    let {name, uName, cell, photo, email, gender, yt, git, tw, lin, fb } = devinput;
+
+console.log(devinput);
+
+
   return (
     <section className='my-3 addDevs '>
         <Container>
@@ -28,7 +47,7 @@ const AddDevs = () => {
                     <Card>
                         <Card.Header> <h3 className='text-center'>Add Developers Data</h3></Card.Header>
                         <Card.Body>
-                        <Form >
+                     
                             <Row>
                                 <Col md={4}>
                                     <Card>
@@ -36,91 +55,100 @@ const AddDevs = () => {
                                         <h4>Preview</h4>
                                             <Row>
                                                 <Col>
-                                                <Card.Img src={"https://www.team.gsamdani.com/wp-content/uploads/2016/05/tm1.jpg"} />
+                                                <Card.Img src={ photo } />
                                                 </Col>
                                                 <Col>
                                                     <span>User Name</span>
-                                                    <h5>ananda44</h5>
+                                                    <h5>{uName}</h5>
                                                     <span>Call</span>
-                                                    <h6>01913918163</h6>
+                                                    <h6>{cell}</h6>
                                                 </Col>
                                             </Row>
                                         </Card.Body>
                                     </Card>
                                     <Card className='anemasion'>
                                         <Card.Body >
-                                        <h5>Name: <p>Ananda Kumar Saha</p></h5>
-                                        <h5>Email: <p>anandasaha484@gmail.com</p></h5>
-                                        <h6>Genger: Male</h6>
+                                        <h5>Name: <p>{ name }</p></h5>
+                                        <h5>Email: <p>{ email }</p></h5>
+                                        <h6>Genger: {gender}</h6>
                                         <h6>Skill: MERN Stack</h6>
                                         <hr />
-                                        <h6><i class='bx bxl-facebook'></i>:               </h6>
-                                        <h6><i class='bx bxl-linkedin' ></i>:               </h6>
-                                        <h6><i class='bx bxl-twitter'></i>:               </h6>
-                                        <h6><i class='bx bxl-github'></i>:               </h6>
-                                        <h6><i class='bx bxl-youtube'></i>:               </h6>
+                                        <h6><i class='bx bxl-facebook'></i>:    {fb}           </h6>
+                                        <h6><i class='bx bxl-linkedin' ></i>:   {lin}            </h6>
+                                        <h6><i class='bx bxl-twitter'></i>:     {tw}          </h6>
+                                        <h6><i class='bx bxl-github'></i>:      {git}         </h6>
+                                        <h6><i class='bx bxl-youtube'></i>:     {yt}          </h6>
                                         </Card.Body>
                                         <div className='anemasionC'>
                                             {/* <ReactLogo></ReactLogo> */}
                                         </div>
-                                        
-                                        
                                     </Card>
-                                    <div className=" header_button">
-                                        <Link  to="/" className='btn btn-primary btn-sm'> Back</Link> &nbsp;
-                                        <Link  to="/admin" className='btn btn-primary btn-sm'> Back to dashbord</Link>
-                                    </div>
                                 </Col>
                                 <Col md={8}>
-                                    <div className="my-1">
-                                        <Form.Label>Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter name" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>User name</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter user name" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Cell No.</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter phone" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                    <div className="my-1">
-                                        <span>Gender</span>
-                                        <div className='mt-0'>
-                                            <input type="radio" id="Male" name='gender' value='Male' /> <label htmlFor="Male">Male</label> &nbsp;
-                                            <input type="radio" id="Female" name='gender'  value='Female' /> <label htmlFor="Female">Female</label>
+                                    <Form >
+                                        <div className="my-1">
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control value={ name } onChange={e => setDevinput(  { ...devinput, name : e.target.value }  )} type="text"  />
                                         </div>
-                                    </div>
-                                    
-                                    <div className="my-1">
-                                        <Form.Label>Twitter</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Twitter</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Github</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Youtube</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                    <div className="my-1">
-                                        <Form.Label>Facebook</Form.Label>
-                                        <Form.Control type="text" placeholder="Enter email" />
-                                    </div>
-                                <Button className='w-100' variant="primary" type="submit">Submit</Button>
+                                        <div className="my-1">
+                                            <Form.Label>User name</Form.Label>
+                                            <Form.Control value={ uName } onChange={e => setDevinput(  { ...devinput, uName : e.target.value }  )} type="text"  />
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Cell No.</Form.Label>
+                                            <Form.Control value={ cell } onChange={e => setDevinput(  { ...devinput, cell : e.target.value }  )} type="text"  />
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Photo</Form.Label>
+                                            <Form.Control value={ photo } onChange={e => setDevinput(  { ...devinput, photo : e.target.value }  )} />
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Email address</Form.Label>
+                                            <Form.Control value={ email } onChange={e => setDevinput(  { ...devinput, email : e.target.value }  )} />
+                                        </div>
+                                        <div className="my-1">
+                                            <span>Gender</span>
+                                            <div className='mt-0'>
+                                                <input type="radio" id="Male" name='gender' value='Male' onChange={e => setDevinput( { ...devinput, gender : e.target.value } )} /> <label htmlFor="Male">Male</label> &nbsp;
+                                                <input type="radio" id="Female" name='gender'  value='Female' onChange={e => setDevinput( { ...devinput, gender : e.target.value } )} /> <label htmlFor="Female">Female</label>
+                                            </div>
+                                        </div>
+                                        
+                                        <div className="my-1">
+                                            <Form.Label>Facebook</Form.Label>
+                                            <Form.Control value={ fb } onChange={e => setDevinput(  { ...devinput, fb : e.target.value }  )} />
+
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Linkedin</Form.Label>
+                                            <Form.Control value={ lin } onChange={e => setDevinput(  { ...devinput, lin : e.target.value }  )} />
+
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Twitter</Form.Label>
+                                            <Form.Control value={ tw } onChange={e => setDevinput(  { ...devinput, tw : e.target.value }  )} />
+
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Github</Form.Label>
+                                            <Form.Control value={ git } onChange={e => setDevinput(  { ...devinput, git : e.target.value }  )} />
+
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Youtube</Form.Label>
+                                            <Form.Control value={ yt } onChange={e => setDevinput(  { ...devinput, yt : e.target.value }  )} />
+
+                                        </div>
+                                        <div className="my-1">
+                                            <Form.Label>Facebook</Form.Label>
+                                            <Form.Control type="text" placeholder="Enter email" />
+                                        </div>
+                                    <Button className='w-100' variant="primary" type="submit">Submit</Button>
+                                    </Form>
                                 </Col>
                             </Row>
                             
-                        </Form>
+                        
                         </Card.Body>
                     </Card>
 
